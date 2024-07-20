bc4f764b0fb05f0f19c8116bffa43007d41b6a04
Yousr <youssrabenlimam88@gmail.com>
2024-07-20 01:12:31 +0100

    simple quiz files

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+    var correctAnswer = '4' ;
+    var selectedRadio = document.querySelector('input[name="quiz"]:checked');
+    var userAnswer = selectedRadio.value;
+
+
+    if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent='Correct! Well done!' ;
+}
+ else {
+    document.getElementById('feedback').textContent=  "That's incorrect. Try again!" ;
+}}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
