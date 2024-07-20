5290f1e6e8b0086302c9c5321ade5c2c54355342
Yousr <youssrabenlimam88@gmail.com>
2024-07-20 02:14:29 +0100

    correction

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer() {
 
 
     if (userAnswer === correctAnswer) {
-        document.getElementById('feedback').textContent='Correct! Well done!' ;
+        document.getElementById('feedback').textContent= "Correct! Well done." ;
 }
  else {
     document.getElementById('feedback').textContent=  "That's incorrect. Try again!" ;
